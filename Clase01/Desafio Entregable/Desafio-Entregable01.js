f8e62b7527971538740174a2d1ff47fb964f0e60
BrunS93 <brunal_003@hotmail.com>
2022-07-14 21:47:48 -0300

    Desafio-Entregable y generico-01

--- Clase01/Desafio Entregable/Desafio-Entregable01.js
+++ Clase01/Desafio Entregable/Desafio-Entregable01.js
@@ -5,8 +5,6 @@ class Usuario{
         this.apellido=apellido;
         this.libros=libros;
         this.mascotas=mascotas;
-
-
     }
 
 
--- Clase01/Desafio Generico/Desafio-Generico3.js
+++ Clase01/Desafio Generico/Desafio-Generico3.js
@@ -1,18 +1,71 @@
-
 class Contador{
-    constructor(){
+    constructor(nombre,apellido, cuentaIndividual){
 
-    }
-
-}
-
-class Persona{
-    constructor(nombre,edad){
         this.nombre=nombre;
-        this.edad=edad;
+        this.apellido=apellido
+        this.cuentaIndividual=cuentaIndividual;
+
+
+
     }
+    obtenerResponsable(){
+        return console.log(`Responsable : ${this.nombre}`);
+    }
+
+    static contadorstatico=0
+
+    obtenerCuentaIndividual(){
+       let conteopersonal=Object.values(this.cuentaIndividual[0])
+        let sumacuenta=0;
+        for(let x=0;x<conteopersonal.length;x++)
+        {
+            sumacuenta+=conteopersonal[x]
+        }
+        Contador.contadorstatico+=sumacuenta
+        return sumacuenta
+    }
+
+    
+    obtenerCuentaGlobal(){
+        return Contador.contadorstatico;
+    }
+
+    contar(){
+        obtenerCuentaIndividual()
+        Contador.contadorstatico++;
+    }
+
 }
 
-const p =new Persona("pepe",5);
 
-console.log(p);
+let cuentadealonso=[
+    {
+    ahorros: 12450,
+    compras:15000,
+    envios: 8500
+    }
+]
+
+let cuentadeMario=[
+    {
+    ahorros: 12450,
+    compras:15000,
+    envios: 8500
+    }
+]
+
+const instanciaMario=new Contador("mario","minchan",cuentadeMario);  // primera instancia
+const marioindv=instanciaMario.obtenerCuentaIndividual();
+console.log(marioindv)
+
+const instanciaAlonso=new Contador("alonso","vazquez",cuentadealonso); // segunda instancia
+const alonsindiv= instanciaAlonso.obtenerCuentaIndividual();
+console.log(alonsindiv);
+
+const global=instanciaAlonso.obtenerCuentaGlobal();
+instanciaAlonso.contar();
+console.log(global)
+// const arr =Contador.contadorstatico;
+// console.log(arr)
+
+
